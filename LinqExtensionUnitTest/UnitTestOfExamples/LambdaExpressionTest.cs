36344d4c2967e83e9324ef1d810a6ed0fe17eac4
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2017-04-24 22:43:03 +0200

    MethodMethodCallExpression

--- LinqExtensionUnitTest/UnitTestOfExamples/LambdaExpressionTest.cs
+++ LinqExtensionUnitTest/UnitTestOfExamples/LambdaExpressionTest.cs
@@ -10,9 +10,9 @@ namespace LinqExtensionUnitTest.UnitTestOfExamples
         [TestMethod]
         public void MethodCallGetParameterName()
         {
-            Expression<Func<int,int>> expr = a => a*a;
-            var parameterExpression=expr.Parameters[0];
-            Assert.AreEqual(parameterExpression.Name,"a");
+            Expression<Func<int, int>> expr = a => a * a;
+            var parameterExpression = expr.Parameters[0];
+            Assert.AreEqual(parameterExpression.Name, "a");
         }
 
         [Ignore]
@@ -20,8 +20,16 @@ namespace LinqExtensionUnitTest.UnitTestOfExamples
         {
             Expression<Func<int, int>> expr = a => a * a;
             var bodyExpression = expr.Body;
-          //  Assert.AreEqual(bodyExpression.GetType(),typeof(System.Linq.Expressions.SimpleBin));
+            //Assert.IsNotNull(bodyExpression as Mul);
         }
 
+        [TestMethod]
+        public void MethodMethodCallExpression()
+        {
+            Expression<Func<string, bool>> expr = a => a.Contains("x");
+            var bodyExpression = expr.Body;
+            Assert.AreEqual(bodyExpression.NodeType, ExpressionType.Call);
+            Assert.IsNotNull(bodyExpression as MethodCallExpression);
+        }
     }
 }
