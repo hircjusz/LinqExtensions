323f5d403d063cf1bc037c722b4025482682aaa8
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2017-04-24 18:48:06 +0200

    MethodCallExpression +CallUpper

---
+++ LinqExtensionUnitTest/UnitTestOfExamples/MethodCallExpressionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqExtensionUnitTest.UnitTestOfExamples
+{
+    [TestClass]
+    public class MethodCallExpressionTest
+    {
+        [TestMethod]
+        public void CallUpper()
+        {
+            var sampleString = "sample string";
+            Expression callExpr = Expression.Call(Expression.Constant(sampleString), typeof(String).GetMethod("ToUpper", new Type[] { }));
+            var lambda = Expression.Lambda<Func<String>>(callExpr).Compile();
+            Assert.AreEqual(sampleString.ToUpper(), lambda.DynamicInvoke());
+        }
+
+
+    }
+}
