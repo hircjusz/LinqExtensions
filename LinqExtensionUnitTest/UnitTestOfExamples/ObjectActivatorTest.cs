7f61f6f105ed906e5b5e6f954b861d32f06cc206
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2017-05-04 16:02:15 +0200

    ObjectActivatorTest +dodanie testu SimpleNewExpressionToCreateType

---
+++ LinqExtensionUnitTest/UnitTestOfExamples/ObjectActivatorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqExtensionUnitTest.UnitTestOfExamples
+{
+
+    //public static class ObjectActivator<T> GetActivator
+
+    [TestClass]
+    public class ObjectActivatorTest
+    {
+
+        [TestMethod]
+        public void SimpleNewExpressionToCreateType()
+        {
+
+            var exp = Expression.New(typeof(Person));
+            var lambda = LambdaExpression.Lambda(exp);
+            object myObj = lambda.Compile().DynamicInvoke();
+
+            Assert.IsNotNull(myObj);
+            Assert.IsInstanceOfType(myObj, typeof(Person));
+        }
+
+
+    }
+}
