3b55951718cfc75186973015d8df0dc331d8e5d0
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2017-05-05 10:50:03 +0200

    StaticReflection

---
+++ LinqExtensionUnitTest/UnitTestOfExamples/StaticReflectionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqExtensionUnitTest.UnitTestOfExamples
+{
+    [TestClass]
+    public class StaticReflectionTest
+    {
+        [TestMethod]
+        public void GetMemberStringLength()
+        {
+            var Length = StaticReflection.GetMemberName<string>(x => x.Length);
+            Assert.AreEqual(nameof(Length), Length);
+        }
+
+    }
+}
